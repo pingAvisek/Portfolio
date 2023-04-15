13b5972eb3175865b7e97e974918f434c5fe36f8
pingAvisek <aghimire074@gmail.com>
2023-04-15 22:31:04 +0545

    init with tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App font-sans">
+      <h1 className="text-black bg-primary">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;

99b07da00d006a03182cda1649b058b40331f572
pingAvisek <aghimire074@gmail.com>
2023-04-24 23:28:26 +0545

    navbar link changed to data.jsx for adding data in future simplicity

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,14 +1,10 @@
 import { Link } from "react-router-dom";
 import Logo from "../components/Logo";
-import {
-  BsGithub,
-  BsTwitter,
-  BsLinkedin,
-  BsTelegram,
-  BsFacebook,
-} from "react-icons/bs";
+import data from "../assets/data";
 
 export default function Header() {
+  const navLinkArray = data[0].navLinks;
+
   return (
     <>
       <div className="header-layout">
@@ -22,37 +18,17 @@ export default function Header() {
                 className="gap-5 text-gray-500 justify-center items-center flex"
                 id="menu"
               >
-                <li className="nav-li hover:text-[#171515]">
-                  <Link to="https://github.com/pingAvisek" target="_blank">
-                    <BsGithub />
-                  </Link>
-                </li>
-                <li className="nav-li hover:text-[#00acee]">
-                  <Link to="https://twitter.com/pingAvisek" target="_blank">
-                    <BsTwitter />
-                  </Link>
-                </li>
-                <li className="nav-li hover:text-primary-700">
-                  <Link
-                    to="https://www.linkedin.com/in/pingavisek/"
-                    target="_blank"
+                {/* Mapping through the navLinkArray */}
+                {navLinkArray.map((navLink, index) => (
+                  <li
+                    className={`nav-li hover:text-[${navLink.color}]`}
+                    key={index}
                   >
-                    <BsLinkedin />
-                  </Link>
-                </li>
-                <li className="nav-li hover:text-[#0088cc]">
-                  <Link to="https://t.me/pingAvisek" target="_blank">
-                    <BsTelegram />
-                  </Link>
-                </li>
-                <li className="nav-li hover:text-[#3b5998]">
-                  <Link
-                    to="https://www.facebook.com/pingAvisek"
-                    target="_blank"
-                  >
-                    <BsFacebook />
-                  </Link>
-                </li>
+                    <Link target="_blank" to={navLink.url}>
+                      {navLink.icons}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </nav>
           </div>

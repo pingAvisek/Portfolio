ee10a9f2d9b7cca434b1f7c706d9e1349c9c7057
pingAvisek <aghimire074@gmail.com>
2023-04-28 23:32:39 +0545

    technology initial commit

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,16 +1,19 @@
-import React from "react";
 import Header from "../pages/Header";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Technology from "../pages/Technology";
+import Test from "../pages/Test";
 
 export default function RootLayout() {
   return (
     <>
       <Header />
-      <Home />
-      <About />
-      <Header />
-      <Home />
+      <main>
+        <Home />
+        <About />
+        <Technology />
+        {/* <Test /> */}
+      </main>
     </>
   );
 }
